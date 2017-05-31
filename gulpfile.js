97ee0c41540b1727ecb6ee9d630dc76bd732ac25
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-06-01 00:33:52 +0530

    Finished Responsive static layout for index.html

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,57 @@ var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin')
 	sass = require('gulp-sass'),
 	cp = require('child_process'),
-	util = require('gulp-util');
+	util = require('gulp-util'),
+	browsersync = require('browser-sync').create(),
+	plumber = require('gulp-plumber');
 
 var paths = {
-	sass: './assets/css/sass/*',
+	sass: './assets/css/sass/**/*.scss',
 	imgIn: './assets/img/*',
 	css: './static/css/',
-	imgOut: './static/img/'
+	imgOut: './static/img/',
+	siteRoot: "_site"
 }
 
 
+gulp.task('serve',function(){
+	// browsersync.init({
+	// 	files: [paths.siteRoot + '/**'],
+	// 	port: 4000,
+	// 	server: {
+	// 		baseDir: paths.siteRoot
+	// 	}
+	// });
+	gulp.watch(paths.sass,['styles']);
+	gulp.watch(paths.imgIn,['images']);
+	// gulp.watch('.',['jekyll']);
+
+});
+
+
+gulp.task('styles', function(){
+	return gulp.src(paths.sass)
+	.pipe(plumber())
+	.pipe(sass({includePaths: require('node-neat').includePaths}))
+	.pipe(gulp.dest(paths.css))
+	.pipe(browsersync.reload({stream: true}));
+
+});
+
+gulp.task('images',function(){
+	return gulp.src(paths.imgIn)
+		.pipe(imagemin())
+		.pipe(gulp.dest(paths.imgOut))
+		.pipe(browsersync.reload({stream: true}));
+});
+
+
+
 gulp.task('jekyll', function(){
-	var jekyll = cp.spawn('jekyll',['serve',
-									'--watch',
-									'--incremental',
-									]);
+	return cp.spawn('jekyll',['serve',
+							'--watch',
+							'--incremental',
+							]);
 	var jekyllLogger = function(buffer){
 		buffer.toString()
 			  .split(/\n/)
@@ -26,26 +62,6 @@ gulp.task('jekyll', function(){
 	};
 });
 
-gulp.task('styles', function(){
-	return gulp.src(paths.sass)
-	.pipe(sass({
-      // includePaths: require('node-neat').with('other/path', 'another/path') 
-      // - or - 
-      includePaths: require('node-neat').includePaths
-    }))
-	.pipe(gulp.dest(paths.css));
 
-});
 
-gulp.task('images',function(){
-	gulp.src(paths.imgIn)
-		.pipe(imagemin())
-		.pipe(gulp.dest(paths.imgOut));
-});
-
-gulp.task('watch',function(){
-	gulp.watch(paths.sass,['styles']);
-	gulp.watch(paths.imgIn,['images']);
-});
-
-gulp.task('default',['images','styles','jekyll','watch']);
+gulp.task('default',['images','styles','jekyll','serve']);
