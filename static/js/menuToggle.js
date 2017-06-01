5993b02d4d1c4d8f84fc3b07f3ce8c938e8dcbc5
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-06-01 09:46:30 +0530

    Updated to use jQuery

---
+++ static/js/menuToggle.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// $('.menu-toggle').click(function(){
+	// 	$('.nav').slideToggle("slow");
+	// });
+//nav_link').css('display', ($('.nav').css("flex-direction") == 'column')? 'block': '');
+});
