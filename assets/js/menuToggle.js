037b6fda9764e8fdf573253749b2103d23857f39
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-06-01 11:36:53 +0530

    Added menu toggle

---
+++ assets/js/menuToggle.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	if ($('.nav').css('flex-direction')=='column'){
+			$('.nav').hide();
+
+	} else if ($('.nav').css('flex-direction')=='row'){
+			$('.nav').show();
+
+	}
+})
+
+$(window).on('resize',function(){
+	if ($('.nav').css('flex-direction')=='column'){
+			$('.nav').hide();
+
+	} else if ($('.nav').css('flex-direction')=='row'){
+			$('.nav').show();
+
+	}
+	console.log($('.nav').css('flex-direction'));
+	
+});
+$('.toggle-button').on('click',function(){
+		$('.nav').slideToggle("fast");
+});
