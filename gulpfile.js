5ccf2ace6756f6f8d7db36c83ce8b812b0d91868
Abhishek Chakraborty <abhishekchak52@gmail.com>
2019-06-06 17:29:45 +0530

    Renamed paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,24 @@ const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const browserSync = require('browser-sync').create();
 
 
 sass.compiler = require('node-sass');
 
 var paths = {
-	jsIn: 'assets/js/*.js',
-	sass: './assets/css/sass/**/*.scss',
-	imgIn: './assets/img/**/*',
-	css: './static/css/',
-	imgOut: './static/img/',
-	jsOut: './static/js/',
+	js: {
+		src: 'assets/js/*.js',
+		dest: './static/js/'
+	},
+	img: {
+		src: './assets/img/**/*',
+		dest: './static/img/'
+	},
+	css: {
+		src: './assets/css/sass/**/*.scss',
+		dest: './static/css/'
+	},
 	siteRoot: "_site"
 }
 
@@ -26,26 +33,42 @@ var messages = {
 
 
 function build_sass() {
-	return src(paths.sass)
+	return src(paths.css.src)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('main.css'))
 		.pipe(cleanCSS())
-		.pipe(dest(paths.css));
-
+		.pipe(dest(paths.css.dest))
+		.pipe(browserSync.stream());
 }
 
 function build_js(){
-	return src(paths.jsIn)
+	return src(paths.js.src)
 		.pipe(concat('main.js'))
 		.pipe(uglify())
-		.pipe(dest(paths.jsOut));
+		.pipe(dest(paths.js.dest))
+		.pipe(browserSync.stream());
 }
 
 function build_img() {
-	return src(paths.imgIn)
+	return src(paths.img.src)
 		.pipe(imagemin())
-		.pipe(dest(paths.imgOut));
+		.pipe(dest(paths.img.dest))
+		.pipe(browserSync.stream());
 }
+
+function serve(){
+	browserSync.init({
+    	files: paths.siteRoot + '/**',
+    	port: 4000,
+        server: {
+            baseDir: '_site'
+        }
+    });
+    watch(paths.css.src, build_sass);
+    watch(paths.js.src, build_js);
+    watch(paths.img.src, build_img);
+}
+
 function clean(cb) {
   // body omitted
   cb();
@@ -59,6 +82,7 @@ function build() {
 
 
 exports.build = build;
+exports.serve = serve;
 exports.default =  series(parallel(build_sass,build_js, build_img), build);
 
 // gulp.task('default', ['browser-sync','watch']);
